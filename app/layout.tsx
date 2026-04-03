00fef30620f80327aeba30250ef395b2f382fc50
tabcodes <tabcodes@tabcodes.dev>
2026-04-03 19:44:51 -0400

    Add coming-soon deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import Header from "./Header";
-
-
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 import "./globals.css";
 
 const jost = Jost({
@@ -23,9 +23,17 @@ export default function RootLayout({
       <body
         className={`${jost.className}`}
       >
-      <Header />        
+        <Header />
+        <div className="flex space-x-8 ml-10">
+          <pre className="mx-auto text-3xl text-slate-500">[coming soon]</pre>
+        </div>
+        <div className="flex space-x-8 ml-10 mt-10">
+            <Link href="https://github.com/tabcodes" className="mx-auto text-5xl text-slate-500 hover:text-slate-700">
+              <FaGithub />
+            </Link>
+        </div>
         <div className="">
-        {children}
+          {children}
 
         </div>
       </body>
