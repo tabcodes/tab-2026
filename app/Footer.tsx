a1a04be4a32a6e2e5ef0e9d47e2d8c33c97c5569
tabcodes <tabcodes@tabcodes.dev>
2026-05-03 16:00:30 -0400

    Footer update

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full py-4 text-center text-sm text-slate-700 bg-slate-500 mt-8">
-      <p>Trademark Theodore Bellas, 2026-Present</p>
+      <p>Copyright Theodore Bellas, 2026-Present</p>
     </footer>
   );
 }
